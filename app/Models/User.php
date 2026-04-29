05009c353a2f134d3290c47c238050a897ab798b
gabreudev <biel0alves0@gmail.com>
2026-04-29 13:08:24 -0300

    feat: models

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'horas_por_dia',
+        'role',
     ];
 
     /**
@@ -45,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function materias()
+    {
+        return $this->hasMany(Materia::class);
     }
 }
