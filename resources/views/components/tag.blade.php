5d84de519ac2be2b2210770d268a4d4a090a1876
Malujoro <mateus_rocha_sousa@hotmail.com>
2026-05-06 15:44:11 -0300

    refactor: tela home

---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,3 @@
+<span {{ $attributes->merge(['class' => 'rounded-full px-4 py-1 font-rem text-[14px] font-medium leading-none']) }}>
+    {{ $slot }}
+</span>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,21 @@
 @section('content')
     <div class="flex flex-col gap-8">
         <div class="flex flex-wrap items-center gap-4">
-            <button class="inline-flex items-center gap-2 rounded-full bg-[#B195D4] px-5 py-2 font-rem text-[16px] font-medium leading-none text-[#2F233B]">
+            <x-button>
                 Gerar Cronograma
                 <span class="inline-flex h-7 w-7 items-center justify-center rounded-full bg-white/70 text-[#6C4A8F]">
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7 2v3M17 2v3M3 9h18M5 6h14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2Z" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path
+                            d="M7 2v3M17 2v3M3 9h18M5 6h14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2Z"
+                            stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                 </span>
-            </button>
+            </x-button>
 
             <div class="flex items-center gap-3">
-                <span class="rounded-full bg-[#B195D4] px-4 py-1 font-rem text-[14px] font-medium leading-none text-[#2F233B]">Teoria</span>
-                <span class="rounded-full bg-[#6BC5D2] px-4 py-1 font-rem text-[14px] font-medium leading-none text-[#114650]">Exercício</span>
-                <span class="rounded-full bg-[#D77979] px-4 py-1 font-rem text-[14px] font-medium leading-none text-[#4E1D1D]">Revisão</span>
+                <x-tag class="bg-[#B195D4] text-[#2F233B]">Teoria</x-tag>
+                <x-tag class="bg-[#6BC5D2] text-[#114650]">Exercício</x-tag>
+                <x-tag class="bg-[#D77979] text-[#4E1D1D]">Revisão</x-tag>
             </div>
         </div>
 
